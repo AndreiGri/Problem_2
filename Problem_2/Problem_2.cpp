29e3494e0c54cf5cf2cb60eafe095ff7991fd77b
Andrei_Gridin <andreigridin918@gmail.com>
2024-06-23 21:11:26 +0500

    Добавьте файлы проекта.

---
+++ Problem_2/Problem_2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+    system("color 80");
+
+    int arr[]{ 11, 25, 32, 48, 55, 68, 75, 81, 9 };
+    int sum = 100;
+    int res = 0;
+
+    for (int i = 0; i < sizeof(arr); i++)
+    {
+        for (int j = i; j < sizeof(arr); j++)
+        {
+            for (int k = 0; k < sizeof(arr) - i; k++)
+            {
+
+            }
+        }
+    }
+
+    system("pause>nul");
+    return 0;
+}
