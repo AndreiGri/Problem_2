4196b711a80bb528b20c9c2ee80647812d1b0696
Andrei_Gridin <andreigridin918@gmail.com>
2024-06-24 20:33:36 +0500

    Без методов

--- Problem_2/Problem_2.cpp
+++ Problem_2/Problem_2.cpp
@@ -3,21 +3,30 @@
 int main()
 {
     system("color 80");
+    system("chcp 1251>nul");
 
-    int arr[]{ 11, 25, 32, 48, 55, 68, 75, 81, 9 };
-    int sum = 100;
+    int arr[]{ 2, 7, 11, 15, 32, 74, -1 };
+    int sum = 81;
     int res = 0;
+    int e = 0;
+    int s = 0;
 
-    for (int i = 0; i < sizeof(arr); i++)
+    for (int i = 0; i < (sizeof(arr) / sizeof(*arr)) - 1; i++)
     {
-        for (int j = i; j < sizeof(arr); j++)
+        for (int j = i + 1; j < (sizeof(arr) / sizeof(*arr)); j++)
         {
-            for (int k = 0; k < sizeof(arr) - i; k++)
+            if (arr[i] + arr[j] == sum)
             {
-
+                std::cout << " Числа: " << arr[i] << " и " << arr[j] << std::endl;
+                e = i;
+                s = j;
+                break;
             }
         }
     }
+
+    std::cout << " С индексами: " << e << " и " << s << std::endl;
+    std::cout << " В сумме составят: " << sum << std::endl;
 
     system("pause>nul");
     return 0;
