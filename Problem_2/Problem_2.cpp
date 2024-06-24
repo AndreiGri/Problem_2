56c1b3a959ac93c335b5a0dc98632ce25eebf311
Andrei_Gridin <andreigridin918@gmail.com>
2024-06-24 21:15:11 +0500

    Добавил коментарии

--- Problem_2/Problem_2.cpp
+++ Problem_2/Problem_2.cpp
@@ -1,9 +1,9 @@
 ﻿#include <iostream>
 
-void calculation_of_the_largest_sum(int s, int& first, int& second, int* arr)
-{
-    for (int i = 0; i < sizeof(arr) - 1; i++)
-    {
+void calculation_of_the_largest_sum(int s, int& first, int& second, int* arr)      // Метод вычисляет заказанную сумму двух чисел любой
+{                                                                                  // последовательности, выводит эти числа в консоль
+    for (int i = 0; i < sizeof(arr) - 1; i++)                                      // и присваивает переменным переданным по ссылке
+    {                                                                              // значения индексов вышеуказанных чисел в массиве
         for (int j = i + 1; j < sizeof(arr); j++)
         {
             if (arr[i] + arr[j] == s)
@@ -22,16 +22,15 @@ int main()
     system("color 80");
     system("chcp 1251>nul");
 
-    int arr[]{ 2, 7, 11, 15, 32, 74, -1 };
-    int sum = 14;
-    int res = 0;
-    int f = 0;
-    int s = 0;
+    int arr[]{ 2, 7, 11, 15, 32, 74, -1 };                                         // Инициализируем массив
+    int sum = 14;                                                                  // Заказанное число
+    int f = 0;                                                                     // Инициализируем переменную для записи индекса первого числа
+    int s = 0;                                                                     // Инициализируем переменную для записи индекса второго числа
 
-    calculation_of_the_largest_sum(sum, f, s, arr);
+    calculation_of_the_largest_sum(sum, f, s, arr);                                // Вызываем метод передав ему необходимые значения
 
-    std::cout << " С индексами: " << f << " и " << s << std::endl;
-    std::cout << " В сумме составят: " << sum << std::endl;
+    std::cout << " С индексами: " << f << " и " << s << std::endl;                 // Выводим в консоль индексы найденных чисел
+    std::cout << " В сумме составят: " << sum << std::endl;                        // Выводим в консоль искомую сумму
 
     system("pause>nul");
     return 0;
