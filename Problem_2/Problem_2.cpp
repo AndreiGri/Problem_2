91a642a68a7951b8375b79d542bbaed6b1d98ad5
Andrei_Gridin <andreigridin918@gmail.com>
2024-06-24 20:50:03 +0500

    Решение вынес в метод

--- Problem_2/Problem_2.cpp
+++ Problem_2/Problem_2.cpp
@@ -1,31 +1,36 @@
 ﻿#include <iostream>
 
+void calculation_of_the_largest_sum(int s, int& first, int& second, int* arr)
+{
+    for (int i = 0; i < sizeof(arr) - 1; i++)
+    {
+        for (int j = i + 1; j < sizeof(arr); j++)
+        {
+            if (arr[i] + arr[j] == s)
+            {
+                std::cout << " Числа: " << arr[i] << " и " << arr[j] << std::endl;
+                first = i;
+                second = j;
+                break;
+            }
+        }
+    }
+}
+
 int main()
 {
     system("color 80");
     system("chcp 1251>nul");
 
     int arr[]{ 2, 7, 11, 15, 32, 74, -1 };
-    int sum = 81;
+    int sum = 14;
     int res = 0;
-    int e = 0;
+    int f = 0;
     int s = 0;
 
-    for (int i = 0; i < (sizeof(arr) / sizeof(*arr)) - 1; i++)
-    {
-        for (int j = i + 1; j < (sizeof(arr) / sizeof(*arr)); j++)
-        {
-            if (arr[i] + arr[j] == sum)
-            {
-                std::cout << " Числа: " << arr[i] << " и " << arr[j] << std::endl;
-                e = i;
-                s = j;
-                break;
-            }
-        }
-    }
+    calculation_of_the_largest_sum(sum, f, s, arr);
 
-    std::cout << " С индексами: " << e << " и " << s << std::endl;
+    std::cout << " С индексами: " << f << " и " << s << std::endl;
     std::cout << " В сумме составят: " << sum << std::endl;
 
     system("pause>nul");
